7cf190ecddc1b93e927deb4afab5729db1e615b6
Cara Hill <cara@rabidtech.co.nz>
2020-01-28 10:11:24 +1300

    First jquery practice question.

---
+++ image_gallery_jquery.js
@@ -0,0 +1,21 @@
+function setup () {
+  $(".remove").on("click", function() {
+    $(this).parent().remove();
+  });
+}
+
+// Example case.
+document.body.innerHTML = `
+<div class="image">
+  <img src="https://goo.gl/kjzfbE" alt="First">
+  <button class="remove">X</button>
+</div>
+<div class="image">
+  <img src="https://goo.gl/d2JncW" alt="Second">
+  <button class="remove">X</button>
+</div>`;
+
+setup();
+
+$(".remove").get(0).click();
+console.log(document.body.innerHTML);
