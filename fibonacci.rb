f170b2fec45fc6d97e770311b37207efa9a468f4
Cara Hill <cara@rabidtech.co.nz>
2020-01-30 11:41:42 +1300

    Added memoization to the recursive method.

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 class Fibonacci
-  def fib(num)
+  def initialize
+    @cached_array = [0, 1]
+  end
+
+  def iterative_fib(num)
     fib_array = (1..num).inject([0, 1]) { |arr| arr << arr.last(2).sum }
     fib_array[num]
+  end
+
+  def recursive_fib(num)
+    return @cached_array[num] if @cached_array[num]
+
+    @cached_array[num] = recursive_fib(num - 1) + recursive_fib(num - 2)
   end
 end
--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -6,13 +6,23 @@ require_relative '../fibonacci'
 RSpec.describe Fibonacci do
   let(:fibonacci) { Fibonacci.new }
 
-  describe 'fib' do
+  describe 'iterative_fib' do
     it 'should return the correct number in the Fibonacci sequence that corresponds to the index of the num argument' do
-      expect(fibonacci.fib(0)).to eq 0
-      expect(fibonacci.fib(2)).to eq 1
-      expect(fibonacci.fib(5)).to eq 5
-      expect(fibonacci.fib(10)).to eq 55
-      expect(fibonacci.fib(20)).to eq 6765
+      expect(fibonacci.iterative_fib(0)).to eq 0
+      expect(fibonacci.iterative_fib(2)).to eq 1
+      expect(fibonacci.iterative_fib(5)).to eq 5
+      expect(fibonacci.iterative_fib(10)).to eq 55
+      expect(fibonacci.iterative_fib(20)).to eq 6765
+    end
+  end
+
+  describe 'recursive_fib' do
+    it 'should return the correct number in the Fibonacci sequence that corresponds to the index of the num argument' do
+      expect(fibonacci.recursive_fib(0)).to eq 0
+      expect(fibonacci.recursive_fib(2)).to eq 1
+      expect(fibonacci.recursive_fib(5)).to eq 5
+      expect(fibonacci.recursive_fib(10)).to eq 55
+      expect(fibonacci.recursive_fib(20)).to eq 6765
     end
   end
 end
