673b9d9b3729898d9a5078599f1ab0caa4cb2daf
Cara Hill <cara@rabidtech.co.nz>
2020-01-30 09:52:24 +1300

    changed naming to alleviate confusion.

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,4 +1,4 @@
 def fib(num)
-  fib_array = (1..num).inject( [0, 1] ) { | fib | fib << fib.last(2).inject(:+) }
+  fib_array = (1..num).inject([0, 1]) { |arr| arr << arr.last(2).inject(:+) }
   fib_array[num]
 end
